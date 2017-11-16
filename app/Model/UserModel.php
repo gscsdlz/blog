7e16de2f7832c6f87e59852facec0762966ec86c
gscsdlz <lz842063523@foxmail.com>
2017-11-16 16:55:36 +0800

    Redis的用户模型

--- app/Model/UserModel.php
+++ app/Model/UserModel.php
@@ -4,11 +4,34 @@
  * User: 南宫悟
  * Date: 2017/11/16
  * Time: 10:42
+ * 用户模型，关联一个用户，与redis进行交互
  */
 
-namespace App\Model\UserModel;
+namespace App\Model;
+
+use Illuminate\Support\Facades\Redis;
 
 class UserModel
 {
+    private $dbname = 'user';  //对应 config/database.php redis的配置
+    private $redis = null; //redis连接
+    private $variable = array(); //绑定用户属性 key->value
+
+    public function __construct()
+    {
+        $this->redis = Redis::connection($this->dbname);
+        $this->variable = $this->redis->hgetall(config('blog.username'));
+    }
+
+    public function __set($name, $value)
+    {
+        $this->variable[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        return isset($this->variable[$name]) ? $this->variable[$name] : null;
+    }
+
 
 }
